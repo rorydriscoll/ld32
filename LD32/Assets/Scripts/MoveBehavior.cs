6231321b81afcedce04d76f92f4beee6149e274c
Rory Driscoll <rorydriscoll@gmail.com>
2015-04-13 20:41:09 -0700

    Add a very simple unity scene with a moving cube as a test to see whether the project opens correctly on other machines

---
+++ LD32/Assets/Scripts/MoveBehavior.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBehavior : MonoBehaviour
+{
+    private float t;
+
+    void Update()
+    {
+        t += Time.deltaTime;
+
+        transform.position = new Vector3(Mathf.Sin(t), 0.5f, 0);
+    }
+}
---
+++ LD32/Assets/Scripts/TrackBehavior.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackBehavior : MonoBehaviour
+{
+    public GameObject trackObject;
+
+    void Update()
+    {
+        Vector3 look = (trackObject.transform.position - transform.position).normalized;
+
+        transform.rotation = Quaternion.LookRotation(look);
+    }
+}
