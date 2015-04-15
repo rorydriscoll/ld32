c207c9ab132ac62d25865c3fff3f92dbe13cc648
Matthew Arrington <matt@Matthews-MacBook-Pro.local>
2015-04-14 19:10:16 -0700

    Test updating project with a new prefab, and script, and sfx

---
+++ LD32/Assets/Scripts/CollisionBehavior.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionBehavior : MonoBehaviour {
+
+	AudioSource audioSource;
+	void Start()
+	{
+		audioSource = GetComponent<AudioSource> ();
+	}
+	void OnCollisionEnter(Collision collision)
+	{
+		if (tag == "ground" || collision.gameObject.tag == "ground")
+			return;
+		Debug.Log ("collision " + gameObject.name +" other = " + collision.gameObject.name);
+		audioSource.Play ();
+
+	}
+}
