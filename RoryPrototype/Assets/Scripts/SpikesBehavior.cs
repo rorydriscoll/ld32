496a1d15c8bb5753749d52857c3a03c840f80f57
Rory Driscoll <rorydriscoll@gmail.com>
2015-04-17 19:33:26 -0700

    Add some materials and make the spikes kill the enemy

---
+++ RoryPrototype/Assets/Scripts/SpikesBehavior.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpikesBehavior : MonoBehaviour
+{
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            DestroyObject(col.gameObject);
+        }
+    }
+}
