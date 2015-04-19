09f8e8ec0f2c255bfc86c0d31a1ae2338679fb06
Rory Driscoll <rorydriscoll@gmail.com>
2015-04-18 20:13:16 -0700

    Use a zombie as the first character

--- LD32/Assets/Scripts/EnemyBehavior.cs
+++ LD32/Assets/Scripts/EnemyBehavior.cs
@@ -16,12 +16,13 @@ public class EnemyBehavior : MonoBehaviour {
 	{
 		identity = ID;
 		Debug.Log ("id = " + ID.ID + " meshid = " + identity.GetMeshID() + " colorid = " + identity.GetColorID());
-		GetComponent<Renderer>().material.color = ID.Color;
+		//GetComponent<Renderer>().material.color = ID.Color;
 		GetComponent<MeshFilter>().mesh = AttributeMesh[identity.GetMeshID()];
 		//Debug.Log ("Speed = " + speed);
-		GetComponent<Rigidbody> ().velocity = -transform.forward * speed; // Random.Range(speedMin, speedMax);
+		GetComponent<Rigidbody> ().velocity = transform.forward * speed; // Random.Range(speedMin, speedMax);
 		initialSpeed = speed;
 		spawner_ =  spawner;
+        transform.rotation = Quaternion.AngleAxis(180, Vector3.up);
 	}
 	void OnCollisionEnter(Collision other)
 	{
@@ -33,7 +34,7 @@ public class EnemyBehavior : MonoBehaviour {
 		float dist = transform.position.z;
 		float decay = speedDecay.Evaluate(dist);
 		//Debug.Log("Dist " + dist + " decay = " + decay);
-		GetComponent<Rigidbody> ().velocity = -transform.forward * initialSpeed * decay; // Random.Range(speedMin, speedMax);
+		GetComponent<Rigidbody> ().velocity = transform.forward * initialSpeed * decay; // Random.Range(speedMin, speedMax);
 		//if (spawner_.IsPlayerDead())
 		//	DestroyObject(gameObject);
 		if (transform.position.z < -20.0f)
