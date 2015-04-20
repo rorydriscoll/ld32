62ec017a7aeab876b264704654d1a99811d45b1d
Rory Driscoll <rorydriscoll@gmail.com>
2015-04-20 00:44:36 -0700

    Add slight spawn delay

--- LD32/Assets/Scripts/Controllers/WeaponController.cs
+++ LD32/Assets/Scripts/Controllers/WeaponController.cs
@@ -5,11 +5,23 @@ using System.Collections.Generic;
 
 public class WeaponController : MonoBehaviour
 {
+    struct Spawner
+    {
+        public Vector3 min;
+        public Vector3 max;
+
+        public float delay;
+
+        public bool fired;
+    };
+
 	public GameObject bomb;
 
-	private bool m_firing;
+	private Spawner[] m_spawners = new Spawner[4];
 	private Identifier m_identifier;
 	private GameController gameController;
+    private float m_timer;
+    private bool m_active;
 
 	void OnDrawGizmos()
 	{
@@ -19,39 +31,72 @@ public class WeaponController : MonoBehaviour
 
 	public void Fire(Identifier identifier)
 	{
-		if (m_firing)
+        if (m_active)
 			return;
+
 		m_identifier = identifier;
-		m_firing = true;
+        m_active = true;
+
+        for (int i = 0; i < m_spawners.Length; ++i)
+        {
+            m_spawners[i].delay = Random.Range(0.0f, 0.5f);
+            m_spawners[i].fired = false;
+        }
 	}
+
 	void Start() 
 	{
 		GameObject gameControllerObj = GameObject.FindWithTag ("GameController");
+
 		if (gameControllerObj != null) 
 			gameController = gameControllerObj.GetComponent<GameController> ();
 		else 
 			Debug.Log ("weapon controller cannot find GameController!");
-	}    
+
+        m_spawners[0].min = new Vector3(-transform.localScale.x, -transform.localScale.y, -transform.localScale.z);
+        m_spawners[0].max = new Vector3(0, transform.localScale.y, 0);
+        m_spawners[1].min = new Vector3(-transform.localScale.x, -transform.localScale.y, 0);
+        m_spawners[1].max = new Vector3(0, transform.localScale.y, transform.localScale.z);
+        m_spawners[2].min = new Vector3(0, -transform.localScale.y, -transform.localScale.z);
+        m_spawners[2].max = new Vector3(transform.localScale.x, transform.localScale.y, 0);
+        m_spawners[3].min = new Vector3(0, -transform.localScale.y, 0);
+        m_spawners[3].max = new Vector3(transform.localScale.x, transform.localScale.y, transform.localScale.z);
+	}
+    
 	void Update()
 	{
-		if (!m_firing || !gameController.CanFireWeapon())
-		{
-			m_firing = false;
+        if (!gameController.CanFireWeapon())
+            m_active = false;
+
+        if (!m_active)
 			return;
-		}
 
-        SpawnBomb(-transform.localScale.x, 0, -transform.localScale.z, 0);
-        SpawnBomb(-transform.localScale.x, 0, 0, transform.localScale.z);
-        SpawnBomb(0, transform.localScale.x, -transform.localScale.z, 0);
-        SpawnBomb(0, transform.localScale.x, 0, transform.localScale.z);
+        m_timer += Time.deltaTime;
 
-		m_firing = false;
+        bool active = false;
+
+        for (int i = 0; i < m_spawners.Length; ++i)
+        {
+            if (m_spawners[i].fired)
+                continue;
+
+            active = true;
+
+            if (m_timer < m_spawners[i].delay)
+                continue;
+            
+            SpawnBomb(m_spawners[i].min, m_spawners[i].max);
+
+            m_spawners[i].fired = true;
+        }
+
+        m_active = active;
 	}
 
-    void SpawnBomb(float xmin, float xmax, float zmin, float zmax)
+    void SpawnBomb(Vector3 min, Vector3 max)
     {
-        float dx = Random.Range(xmin, xmax) * 0.5f;
-        float dz = Random.Range(zmin, zmax) * 0.5f;
+        float dx = Random.Range(min.x, max.x) * 0.5f;
+        float dz = Random.Range(min.z, max.z) * 0.5f;
 
         GameObject go = GameObject.Instantiate<GameObject>(bomb);
 
