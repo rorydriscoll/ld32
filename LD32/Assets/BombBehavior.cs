b8f78e47366cfa5886ffc2076f9a1246029efa2b
Rory Driscoll <rorydriscoll@gmail.com>
2015-04-19 22:44:32 -0700

    Add crappy sounds

--- LD32/Assets/BombBehavior.cs
+++ LD32/Assets/BombBehavior.cs
@@ -6,6 +6,9 @@ public class BombBehavior : MonoBehaviour
     public float radius = 10.0f;
     public Mesh[] variations;
     public GameObject fragment;
+    public AudioClip deploySound;
+    public AudioClip hitSound;
+    public AudioClip[] explodeSounds;
 
     private float m_ttl;
     private Identifier m_identifier;
@@ -13,6 +16,12 @@ public class BombBehavior : MonoBehaviour
     void Start()
     {
         m_ttl = Random.Range(1.0f, 2.0f);
+
+        if (deploySound)
+        {
+            GetComponent<AudioSource>().pitch = Random.Range(0.9f, 1.1f);
+            GetComponent<AudioSource>().PlayOneShot(deploySound);
+        }
     }
 
     void SetIdentifier(object o)
@@ -49,13 +58,19 @@ public class BombBehavior : MonoBehaviour
             SpawnFragment(transform.position + new Vector3(-offset, 1, -offset));
             SpawnFragment(transform.position + new Vector3(-offset, 1, offset));
             SpawnFragment(transform.position + new Vector3(offset, 1, -offset));
-            SpawnFragment(transform.position + new Vector3(offset, 1, offset));
+            GameObject fragment = SpawnFragment(transform.position + new Vector3(offset, 1, offset));
+
+            if (explodeSounds.Length > 0)
+            {
+                fragment.GetComponent<AudioSource>().pitch = Random.Range(0.8f, 0.9f);
+                fragment.GetComponent<AudioSource>().PlayOneShot(explodeSounds[Random.Range(0, explodeSounds.Length - 1)]);
+            }
 
             DestroyObject(gameObject);
         }
     }
 
-    void SpawnFragment(Vector3 position)
+    GameObject SpawnFragment(Vector3 position)
     {
         GameObject go = GameObject.Instantiate<GameObject>(fragment);
 
@@ -65,5 +80,16 @@ public class BombBehavior : MonoBehaviour
         Vector3 direction = (position - transform.position + Vector3.up).normalized;
 
         go.GetComponent<Rigidbody>().velocity = direction * 70;
+
+        return go;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (hitSound != null)
+        {
+            GetComponent<AudioSource>().pitch = Random.Range(0.8f, 1.2f);
+            GetComponent<AudioSource>().PlayOneShot(hitSound);
+        }
     }
 }
